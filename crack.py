a5287f5315a46c7116e746c57c2b1068509a0612
Xander Stokes <37290813+Skwangles@users.noreply.github.com>
2023-09-07 04:08:42 +0000

    fixup crack.py and format

--- crack.py
+++ crack.py
@@ -1,69 +1,70 @@
+import sys
+
+
 def crack(args):
     if len(args) != 2:
         print_help()
         return
-    print(args)
     match args[0].lower():
         case "rsa":
-          # insert calls to RSA cracker, etc
-          break
+            # insert calls to RSA cracker, etc
+            pass
         case "c":
-          # insert calls to Caesar cipher cracker, etc
-          break
+            # insert calls to Caesar cipher cracker, etc
+            pass
         case "ss":
-          # insert calls to Simple Substitution cracker, etc
-          break
-        # Insert whatever extra ciphers we choose here
+            # insert calls to Simple Substitution cracker, etc
+            pass
+            # Insert whatever extra ciphers we choose here
         case _:
-          print("Not a supported cipher type")
-          print_help()
+            print("Not a supported cipher type")
+            print_help()
 
 
 def encrypt(args):
-  if len(args) != 3:
+    if len(args) != 3:
         print_help()
         return
-    
     match args[0].lower():
         case "rsa":
-          # insert calls to RSA cracker, etc
-          break
+            # insert calls to RSA cracker, etc
+            pass
         case "c":
-          # insert calls to Caesar cipher cracker, etc
-          break
+            # insert calls to Caesar cipher cracker, etc
+            pass
         case "ss":
-          # insert calls to Simple Substitution cracker, etc
-          break
+            # insert calls to Simple Substitution cracker, etc
+            pass
         # Insert whatever extra ciphers we choose here
         case _:
-          print("Not a supported cipher type")
-          print_help()
+            print("Not a supported cipher type")
+            print_help()
 
 
-def decrypt(type, text, key):
-   if len(args) != 3:
+def decrypt(args):
+    if len(args) != 3:
         print_help()
         return
-     
+
     match args[0].lower():
         case "rsa":
-          # insert calls to RSA cracker, etc
-          break
+            # insert calls to RSA cracker, etc
+            pass
         case "c":
-          # insert calls to Caesar cipher cracker, etc
-          break
+            # insert calls to Caesar cipher cracker, etc
+            pass
         case "ss":
-          # insert calls to Simple Substitution cracker, etc
-          break
+            # insert calls to Simple Substitution cracker, etc
+            pass
         # Insert whatever extra ciphers we choose here
         case _:
-          print("Not a supported cipher type")
-          print_help()
-  
+            print("Not a supported cipher type")
+            print_help()
+
 
 def print_help():
-  print("Usage: python crack.py <c-crack|e-encrypt|d-decrypt> <cipher-type> <ciphertext>")
-  print("""
+    print("Usage: python crack.py <c-crack|e-encrypt|d-decrypt> <cipher-type> <ciphertext>")
+    print("""
         Cipher type options: 
           RSA - RSA encryption, 
           C - Caesar Cipher 
@@ -71,22 +72,25 @@ def print_help():
         
         e.g. python crack.py c ss abcdefg
         """)
-            
-if __name == "__main__":
+
+
+def main():
     args = sys.argv
     # Verify args
     if len(args) <= 1:
-      print_help()
-      print("Please provide one or more arguments.")
-      return
-      
-    # Determine operation  
+        print_help()
+        print("Please provide one or more arguments.")
+        return
+
+    # Determine operation
     match args[1].lower():
         case "e":
-          encrypt(args[2:])
+            encrypt(args[2:])
         case "d":
-          decrypt(args[2:])
+            decrypt(args[2:])
         case "c":
-          crack(args[2:])
-    
-      
+            crack(args[2:])
+
+
+if __name__ == "__main__":
+    main()
