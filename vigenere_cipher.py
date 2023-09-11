5dc060d9370729bdb8bf445e1e7c50c7968ae825
0n3Appl3 <jedd.lupoy@gmail.com>
2023-09-11 16:03:58 +1200

    Implemented encrypt and decrypt

--- vigenere_cipher.py
+++ vigenere_cipher.py
@@ -1,18 +1,44 @@
-def encrypt(text, key):
-    keySequence = generateKeySequence(key, text)
-    # print(keySequence)
-    """Encrypts the text using the key"""
-    return "Not implemented"
+#
+# Encrypts the text using key.
+#
+def encrypt(text: str, key: str):
+    cipher_text = ""
 
-def decrypt(cypher_text, key):
-    """Decrypts the cypher text using the key"""
-    return "Not implemented"
+    x = range(len(text))
+    for i in x:
+        m = ord(text[i].upper())
+        k = ord(key[i].upper())
+        char = ((m + k) % 26) + ord('A')
+        cipher_text = cipher_text + chr(char)
+    print(cipher_text)
+    return cipher_text
 
+#
+# Decrypts the cypher text using key.
+#
+def decrypt(cypher_text: str, key: str):
+    original_text = ""
+
+    x = range(len(cypher_text))
+    for i in x:
+        char = ord(cypher_text[i].upper())
+        k = ord(key[i].upper())
+        m = ((char - k) % 26) + ord('A')
+        original_text = original_text + chr(m)
+    print(original_text)
+    return original_text
+
+#
+# Cracks the cypher text, returning the key.
+#
 def crack(cypher_text):
     """Cracks the cypher text, returning the key"""
     return "Not implemented"
 
-def generateKeySequence(key, text):
+#
+# Generate the key sequence in a cyclic manner from key.
+#
+def generate_key_sequence(text: str, key: str):
     seq = list(key)
     if (len(seq) == len(text)):
         return key
@@ -24,4 +50,8 @@ def generateKeySequence(key, text):
     return "" . join(seq)
 
 # if __name__ == "__main__":
-#     encrypt("hellothere", "apps")
+#     text = "hiandwelcometomycypher"
+#     key = "crypto"
+#     key_seq = generate_key_sequence(text, key)
+#     cypher_text = encrypt(text, key_seq)
+#     original_text = decrypt(cypher_text, key_seq)
