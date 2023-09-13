f41439c29b374c47e7fe6c8e2c1a8c0f5709b1cd
Greasypizzatoppings <baxter@baxterhills.com>
2023-09-13 21:40:41 +1200

    implement encrypt and decrypt for alphanumeric characters

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,10 +1,35 @@
 def encrypt(text, key):
     """Encrypts the text using the key"""
-    return "Not implemented"
+    
+    encrypted = ""
+    for char in text:
 
-def decrypt(cypher_text, key):
+        if char.isalnum():
+            # lowercase by default but handle uppercase and digits
+            baseChar = 'a'
+            setLength = 26
+
+            if char.isupper():
+                baseChar = 'A'
+
+            elif char.isdigit():
+                baseChar = '0'
+                setLength = 10
+        
+            offset = (ord(char) - ord(baseChar) + int(key)) % setLength
+            encrypted += chr(offset + ord(baseChar))
+        
+        #leave non-alphanumeric characters unchanged
+        else: encrypted += char
+            
+    return encrypted
+
+
+def decrypt(cipher_text, key):
     """Decrypts the cypher text using the key"""
-    return "Not implemented"
+    #encrypting with the negative key is the same as decrypting
+    return encrypt(cipher_text, -int(key)) 
+
 
 def crack(cypher_text):
     """Cracks the cypher text, returning the key"""
