516a44d4352a06b0810d0e9ebeb72844c358ee97
Greasypizzatoppings <baxter@baxterhills.com>
2023-09-14 19:29:49 +1200

    implement english word counting to sort output to find more probable decrypt key

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,3 +1,7 @@
+import nltk
+from nltk.corpus import words 
+
+
 def encrypt(text, key):
     """Encrypts the text using the key"""
     
@@ -5,7 +9,7 @@ def encrypt(text, key):
     for char in text:
 
         if char.isalnum():
-            # lowercase by default. also handle uppercase and digits 0-9
+            #lowercase by default. also handle uppercase and digits 0-9
             baseChar = 'a'
             setLength = 26
 
@@ -22,7 +26,7 @@ def encrypt(text, key):
         #leave non-alphanumeric characters unchanged
         else: encrypted += char
             
-    return encrypted
+    return "'" + encrypted + "'" #surround in single quotes to show any whitespace at the end or start
 
 
 def decrypt(cipher_text, key):
@@ -32,19 +36,32 @@ def decrypt(cipher_text, key):
     return encrypt(cipher_text, -int(key)) 
 
 
-#uses brute force, but a TODO could be to also use letter frequency analysis on these to select the most probable text/key
+#uses brute force and english word counting to sort the output by most probable to least probable decrypt key
 def crack(cipher_text):
-    """Cracks the cipher text using brute force, returning all the possible texts and keys. The user must identify the correct one."""
+    """Cracks the cipher text using brute force, returning all the possible texts and keys, sorted by number of matching english words"""
     
-    shifts = ""
+    #get english words if not already downloaded
+    if not nltk.data.find('corpora/words.zip'):
+        nltk.download('words')
+    englishWords = set(words.words())
 
-    #optimisation: if theres no alpha character, only need to test for digits
+    #optimisation: if theres no alpha characters, only need to crack for digits
     if any(char.isalpha() for char in cipher_text): setLength = 26
     else: setLength = 10
 
     #try all possible shifts
+    results = []
     for key in range(1,setLength):  
-            decrypted_text = decrypt(cipher_text, key)
-            shifts += (f"key {key}: '{decrypted_text.encode('utf-8').decode('utf-8', 'ignore')}'") + "\n"
+        decryptedText = decrypt(cipher_text, key)[1:-1] #test decrypt with that key. remove first and last apostrophe
+        decryptedWords = decryptedText.split()
 
-    return shifts[:-1] #take away last newline
+        #count number of matching english words
+        englishWordCount = sum(1 for word in decryptedWords if word.lower() in englishWords)
+        results.append((key, decryptedText, englishWordCount))
+
+    #sort by english word count descending
+    results.sort(key=lambda result: result[2], reverse=True) 
+
+    #make the results more readable for console output
+    readableResults = '\n'.join(f"key: {key}\t\ttext: '{text}'\t\tenglish words: {wordCount}" for key, text, wordCount in results)
+    return readableResults
