aa867af692e2f92ff27598c7a1bdc2dc3177ede1
Xander Stokes <xander.c.stokes@gmail.com>
2023-09-15 22:38:37 +1200

    Switch to using parameters instead of args

--- rsa.py
+++ rsa.py
@@ -1,25 +1,17 @@
-def encrypt(args):
+def encrypt(text, key):
     """Encrypts the text using the key"""
-    text = str(args.text)
-    key = str(args.key)
     if not text or not key:
         return "Missing required arguments for this function"
-    
     return "Not implemented"
 
-def decrypt(args):
+def decrypt(cypher_text, key):
     """Decrypts the cypher text using the key"""
-    cypher_text = str(args.text)
-    key = str(args.key)
     if not cypher_text or not key:
         return "Missing required arguments for this function"
-    
     return "Not implemented"
 
-def crack(args):
+def crack(cypher_text):
     """Cracks the cypher text, returning the key"""
-    cypher_text = str(args.text)
     if not cypher_text:
         return "Missing required arguments for this function"
-    
     return "Not implemented"
