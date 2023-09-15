7d94f767e831a1f6ba69fb17e696de9088d63114
Xander Stokes <xander.c.stokes@gmail.com>
2023-09-15 22:03:34 +1200

    Switch unimplemented to new args format

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,11 +1,25 @@
-def encrypt(text, key):
+def encrypt(args):
     """Encrypts the text using the key"""
+    text = str(args.text)
+    key = str(args.key)
+    if not text or not key:
+        return "Missing required arguments for this function"
+    
     return "Not implemented"
 
-def decrypt(cypher_text, key):
+def decrypt(args):
     """Decrypts the cypher text using the key"""
+    cypher_text = str(args.text)
+    key = str(args.key)
+    if not cypher_text or not key:
+        return "Missing required arguments for this function"
+    
     return "Not implemented"
 
-def crack(cypher_text):
+def crack(args):
     """Cracks the cypher text, returning the key"""
+    cypher_text = str(args.text)
+    if not cypher_text:
+        return "Missing required arguments for this function"
+    
     return "Not implemented"
