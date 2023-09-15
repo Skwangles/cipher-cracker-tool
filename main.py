965495ebce8717fb13f70592bc36838a4b1c80e7
Xander Stokes <xander.c.stokes@gmail.com>
2023-09-15 22:03:08 +1200

    Argparse cmd line structure - more robust method

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sys
+import argparse
+
 
 # Cipher modules
 import caesar_cipher
@@ -7,90 +9,72 @@ import simple_substitution
 import feistel_cipher
 
 ### --- Cipher calls
-def call_rsa(args):
-    match args[0].lower():
+def call_cipher(cipher, args):
+    match args.action.lower():
         case "crack":
-            print(rsa.crack(args[1]))
+            print(cipher.crack(args))
         case "decrypt":
-            print(rsa.decrypt(*args[1:]))
+            print(cipher.decrypt(args))
         case "encrypt":
-            print(rsa.encrypt(*args[1:]))
+            print(cipher.encrypt(args))
         case _:
-            print("Unsupported operation:", args[0])
-            print_help()
+            print("Unsupported operation:", args.action)
 
-def call_caesar_cipher(args):
-    match args[0].lower():
-        case "crack":
-            print(caesar_cipher.crack(args[1]))
-        case "decrypt":
-            print(caesar_cipher.decrypt(*args[1:]))
-        case "encrypt":
-            print(caesar_cipher.encrypt(*args[1:]))
-        case _:
-            print("Unsupported operation:", args[0])
-            print_help()
+### ----- Arguments handling functions -----
 
-def call_simple_substitution(args):
-    match args[0].lower():
-        case "crack":
-            print(simple_substitution.crack(args[1]))
-        case "decrypt":
-            print(simple_substitution.decrypt(*args[1:]))
-        case "encrypt":
-            print(simple_substitution.encrypt(*args[1:]))
-        case _:
-            print("Unsupported operation:", args[0])
-            print_help()
+# define command line arguments and subparsers
+parser = argparse.ArgumentParser(prog="main.py", description="Encrypt, decrypt or crack a cipher text.")
 
-def call_fiestel_cipher(args):
-    match args[0].lower():
-        case "crack":
-            print(feistel_cipher.crack(args[1]))
-        case "decrypt":
-            print(feistel_cipher.decrypt(*args[1:]))
-        case "encrypt":
-            print(feistel_cipher.encrypt(*args[1:]))
-        case _:
-            print("Unsupported operation:", args[0])
-            print_help()
-            
-### ----- General functions -----
+# required cipher type first
+individual_cipher_arg_parsers = parser.add_subparsers(title="cipher", dest="cipher", required=True, help="Cipher types - rsa, caesar, simple, feistel")
 
-def print_help():
-    print("Usage: python main.py <cipher-type> <crack|encrypt|decrypt> <ciphertext> <cipher specific args>")
-    print("""
-        Cipher type options: 
-          RSA encryption - args: rsa <crack|encrypt|decrypt> <ciphertext> <public key> <(enc/dec modes)private key>
-          Caesar Cipher - args: caesar <crack|encrypt|decrypt> <ciphertext> <shift>
-          Simple Substitution - args: simple <crack|encrypt|decrypt> <ciphertext> <key>
-          Fietsel Cipher - args: "feistel" <crack|encrypt|decrypt> <ciphertext> <keys(multiple of keylength)> <keylength> <rounds>
-        e.g. python main.py simple crack abcdefg
-        """)
+# give selection of actions - encrypt, decrypt, crack
+action_group = parser.add_argument_group("action", argument_default="encrypt")
+action_group.add_argument("--encrypt", "-e", action="store_const", const="encrypt", dest="action", default="encrypt", help="Encrypt the plain text")
+action_group.add_argument("--decrypt", "-d", action="store_const", const="decrypt", dest="action", help="Decrypt the cipher text")
+action_group.add_argument("--crack", "-c", action="store_const", const="crack", dest="action", help="Crack the cipher text")
 
+# caesar - shift based key 0-26
+caesar_parser = individual_cipher_arg_parsers.add_parser("caesar", help="Caesar Cipher")
+caesar_parser.add_argument("-t","--text", required=True, help="*Text to encrypt/decrypt/crack")
+caesar_parser.add_argument("-s", "--shift", default=0, type=int, choices=range(0, 27), help="Caesar shift (0-26)")
+
+# rsa
+rsa_parser = individual_cipher_arg_parsers.add_parser("rsa", help="RSA Cipher")
+rsa_parser.add_argument("-t","--text", required=True, help="*Text to encrypt/decrypt")
+rsa_parser.add_argument("-k", "--private", help="Private key")
+rsa_parser.add_argument("-p", "--public", help="Public key")
+
+# simple sub - text based key
+simple_parser = individual_cipher_arg_parsers.add_parser("simple", help="Simple Substitution Cipher")
+simple_parser.add_argument("-t","--text", required=True, help="*Text to encrypt/decrypt")
+simple_parser.add_argument("-k", "--key", default="abcdefghijklmnopqrstuvwxyz", help="Substitution key - default: 'abcdefghijklmnopqrstuvwxyz'")
+
+# feistel - text based key, rounds integer, and keylength integer
+feistel_parser = individual_cipher_arg_parsers.add_parser("feistel", help="Feistel Cipher")
+feistel_parser.add_argument("-t","-b","--text", "--binary", required=True, dest="text", help="Plaintext to encrypt/Binary to decrypt")
+feistel_parser.add_argument("-k", "--keys", default="abc123hij", help="Key string - default: 'abc123hij' (w/ keylength 3) ")
+feistel_parser.add_argument("-l", "--keylength", default=3, help="# chars in individual round keys - the key string's length must be a multiple of this number - default: 3")
+feistel_parser.add_argument("-r", "--rounds", default=16, help="# of rounds to run the feistel cipher for - default: 16")
 
 def main():
-    prog_args = sys.argv
-    # Verify args
-    if len(prog_args) <= 1:
-        print_help()
-        print("Please provide one or more arguments.")
-        return
+    prog_args = parser.parse_args()
 
-    match prog_args[1].lower():
+    cipher_type = None
+    match prog_args.cipher.lower():
         case "rsa":
-            call_rsa(prog_args[2:])
+            cipher_type = rsa
         case "caesar":
-            call_caesar_cipher(prog_args[2:])
+            cipher_type = caesar_cipher
         case "simple":
-            call_simple_substitution(prog_args[2:])
+            cipher_type = simple_substitution
         case "feistel":
-            call_fiestel_cipher(prog_args[2:])
-        # Insert whatever extra ciphers we choose here
+            cipher_type = feistel_cipher
         case _:
-            print("Not a supported cipher type")
-            print_help()
-
+            print("Unsupported cipher type:", prog_args.cipher)
+            sys.exit(1)
+                 
+    call_cipher(cipher_type, prog_args)
 
 if __name__ == "__main__":
     main()
