f26a6c90fc2504cebc5d803a717590a6cad74c80
Greasypizzatoppings <baxter@baxterhills.com>
2023-09-17 19:37:26 +1200

    moved get_english_percent() to utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
-import random
+import random, nltk
+from nltk.corpus import words 
 
 def index_of_coincidence(input):
     """Calculates the IoC"""
@@ -49,3 +50,31 @@ def random_key(length=3):
     """Generates a random key of the given length"""
     alphabet = "abcdefghijklmnopqrstuvwxyz0123456789"
     return "".join(random.choice(alphabet.ascii_lowercase) for i in range(length))
+
+
+#store the set of words globally so we dont have to spend ages getting them each time
+english_words = None 
+
+def get_english_percent(input):
+    """Calculates the percentage of english words in a text"""
+
+    if not input:
+        print("Missing input text to calculate english word %")
+        return None
+
+    #if our global set of english words isnt available yet, go get them
+    global english_words
+    if not english_words:
+        try:
+            nltk.data.find('corpora/words.zip')
+        except LookupError:
+            nltk.download('words')
+
+        english_words = set(words.words())
+    
+
+    input_words = input.split()    
+        
+    #search our text for english words
+    english_word_count = sum(1 for word in input.split() if word.lower() in english_words)
+    return round((english_word_count / len(input_words)) * 100, 2)
