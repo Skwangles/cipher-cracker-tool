62be28c18a767db2ee5e7b13a3bfb20a31aad11e
Xander Stokes <xander.c.stokes@gmail.com>
2023-09-28 15:53:05 +1300

    moving to usage of pow for %, and fix usage of modinv p-1

--- massey_omura.py
+++ massey_omura.py
@@ -3,33 +3,32 @@ import utils
 def encrypt(m, a, b, p):
     """Encrypts the text using the key"""
     # Get the inverse of a and b - private information
-    a_inv = utils.get_modular_inv(a, p) 
-    b_inv = utils.get_modular_inv(b, p) 
-    if a_inv == -1 or b_inv == -1:
-        return "No inverse found"
+    a_inv = utils.modInverse(a, p-1) 
+    b_inv = utils.modInverse(b, p-1) 
     
-    # Calculate the cipher text
-    m_a = pow(m, a) % p
+    # Step 1 - Alice sends m to Bob
+    m_a = pow(m, a, p)
     
-    # Bob sends m_ab to Alice
-    m_ab = agree_on_key(m_a, b, p)
+    # Step 2 - Alice sends m_a to Bob
+    m_ab = pow(m_a, b, p)
     
-    # Alice sends m_b to Bob - this is the cipher text, and can be decrypted by Bob's private key b
-    m_b = pow(m_ab, a_inv) % p
+    # Step 4 - Alice multiplies by her inverse to get m_b
+    m_b = pow(m_ab, a_inv, p)
+    
 
-    print("m^a:", m_a)
-    print("m^ab:", m_ab)
+    print("y1:", m_a)
+    print("y2:", m_ab)
     print("p:", p)
-    print("Cipher Text to bob:")
+    print("Cipher Text to bob (y3):")
     return m_b
 
 
 def decrypt(m_b, b, p, b_inv=-1):
     """Decrypts the cypher text using the m^b"""
     if b_inv == -1:
-        b_inv = utils.get_modular_inv(b, p)
+        b_inv = utils.modInverse(b, p-1)
         
-    return pow(m_b, b_inv) % p
+    return pow(m_b, b_inv, p)
 
 
 def crack(m_a, m_ab, m_b, p):
@@ -39,15 +38,10 @@ def crack(m_a, m_ab, m_b, p):
     if a_mod_p == -1 or b_mod_p == -1:
         return "Could not determine a or b (e.g. m^a mod p) in a reasonable time - please use smaller numbers if you want to crack"
 
-    print("alice key:", a_mod_p)
-    print("bob key:", b_mod_p)
+    print("A key:", a_mod_p)
+    print("B key:", b_mod_p)
 
     return pow(m_b, b_mod_p) % p
-
-
-def agree_on_key(base, exp, p):
-    """Agrees on a value"""
-    return pow(base, exp) % p
 
 
 def find_key(m_a, m_ab, p):
@@ -56,17 +50,31 @@ def find_key(m_a, m_ab, p):
         return 1
     
     for i in range(2, p):
-        if (pow(m_a, i) % p) == m_ab:
+        if (pow(m_a, i, p)) == m_ab:
             return i
     return -1
 
 if __name__ == "__main__":
+    
     print("Running Massey-Omura")
-    print("Encrypting 5 with a=3, b=7, p=11")
-    result = encrypt(5, 3, 7, 11)
+    
+    # Worked example
+    # step 1 - ma = 7
+    # step 2 - mab = 12
+    # step 3 - mb = 11
+    # step 4 - m = 3
+    
+    m = 3
+    a = 11
+    b = 7
+    p = 17
+    
+    print("### Encrypting: ", m, "###")
+    result = encrypt(m=m, a=a, b=b, p=p)
     print(result)
    
-    print("Decrypting result")
+    print("### Decrypting result ###")
     print(decrypt(11, b=b, p=p))
-    print("Cracking")
+    
+    print("### Cracking ###")
     print(crack(7, 12, 11, 17))
