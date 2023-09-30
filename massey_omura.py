3af0bc5ba0dc4618a32b145f786a833c5206d1ca
skwangles <tree_house@Hotmail.co.nz>
2023-09-30 15:39:05 +1300

    Switch back to worked example values

--- massey_omura.py
+++ massey_omura.py
@@ -64,16 +64,10 @@ if __name__ == "__main__":
     # step 3 - mb = 11
     # step 4 - m = 3
     
-    # m = 3
-    # a = 11
-    # b = 7
-    # p = 17
-    
-    # Assignment
-    m = 10
-    a = 3
-    b = 5
-    p =17
+    m = 3
+    a = 11
+    b = 7
+    p = 17
     
     print("### Encrypting: ", m, "###")
     result = encrypt(m=m, a=a, b=b, p=p)
