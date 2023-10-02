9f622a83280461d3c45e997eed0a9eb235b4461f
Xander Stokes <xander.c.stokes@gmail.com>
2023-10-03 10:33:09 +1300

    IoC now accepts 'non-alpha' strings

--- utils.py
+++ utils.py
@@ -3,23 +3,26 @@ import random
 def index_of_coincidence(input):
     """Calculates the IoC"""
 
-    if not input.isalpha():
-        print("Called IoC on non-alpha string")
-        return None
-
     # Sum occurances of letters
     letters = {}
+    len_alpha_input = 0
     for i in input:
-        if i in letters:
-            letters[i] += 1
-        else:
-            letters[i] = 1
+        if str(i).isalpha():
+            len_alpha_input += 1
+            if i.upper() in letters:
+                letters[i] += 1
+            else:
+                letters[i] = 1
+
+    if len_alpha_input == 0:
+        # No alpha chars to count
+        return 0
 
     # Calculate using algorithm from class
     sigma_f = 0
     for f in letters.items():
         sigma_f += f[1] * (f[1] - 1)
-    big_n = len(input)
+    big_n = len_alpha_input
     return sigma_f/(big_n * (big_n - 1))
 
 
