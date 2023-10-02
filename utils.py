b3504ff57879012cb61799b0264341a6d14cd98f
skwangles <tree_house@Hotmail.co.nz>
2023-10-02 17:32:00 +1300

    add modinv from massey-omura

--- utils.py
+++ utils.py
@@ -75,3 +75,35 @@ def frequency_analysis(cipher_text):
     sorted_freq = ''.join(sorted_freq)
 
     return sorted_freq
+
+def modInverse(A, M):
+    # From https://www.geeksforgeeks.org/multiplicative-inverse-under-modulo-m/ - Daniel confirmed okay to use
+    m0 = M
+    y = 0
+    x = 1
+ 
+    if (M == 1):
+        return 0
+ 
+    while (A > 1):
+ 
+        # q is quotient
+        q = A // M
+ 
+        t = M
+ 
+        # m is remainder now, process
+        # same as Euclid's algo
+        M = A % M
+        A = t
+        t = y
+ 
+        # Update x and y
+        y = x - q * y
+        x = t
+ 
+    # Make x positive
+    if (x < 0):
+        x = x + m0
+ 
+    return x
