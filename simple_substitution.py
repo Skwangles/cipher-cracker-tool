8963c1d78b7bef7536915c438e0843dc0b0c7038
skwangles <tree_house@Hotmail.co.nz>
2023-10-16 21:19:46 +1300

    add alive_bar for each cypher text word
    having a single status, rather than like 500
    is more useful and meaningful
    
    important to make sure indentation is right

--- simple_substitution.py
+++ simple_substitution.py
@@ -157,29 +157,28 @@ def crack(cypher_text):
     
     # check longest words first as they have the most information
     encrypted_words = sorted(encrypted_words, key=len, reverse=True)
-    
-    for c_word in encrypted_words:
-        pattern = get_pattern_of_unique(c_word)
-        
-        if pattern not in word_patterns:
-            continue
-        
-        if len(word_patterns[pattern]) == 1:
-            # we have found the word, so add it to the mapping
-            for i in range(len(c_word)):
-                mapping[c_word[i]] = [word_patterns[pattern][0][i]]
-            continue
-        
-        single_word_map = get_empty()
-        with alive_bar(len(word_patterns[pattern])) as bar:
+    with alive_bar(len(encrypted_words)) as bar:
+        for c_word in encrypted_words:
+            pattern = get_pattern_of_unique(c_word)
+            
+            if pattern not in word_patterns:
+                continue
+            
+            if len(word_patterns[pattern]) == 1:
+                # we have found the word, so add it to the mapping
+                for i in range(len(c_word)):
+                    mapping[c_word[i]] = [word_patterns[pattern][0][i]]
+                continue
+            
+            single_word_map = get_empty()
             for possible_word in word_patterns[pattern]:
                 for i in range(len(c_word)):
                     if c_word[i] in single_word_map:
                         letter = possible_word[i]
                         if letter not in single_word_map[c_word[i]]:
                             single_word_map[c_word[i]].append(letter)
-                bar()
-        mapping = addGuessesToMap(mapping, single_word_map)
+            bar()
+            mapping = addGuessesToMap(mapping, single_word_map)
     
     mapping = collapse_solved_letters(mapping)
     print(mapping)
@@ -259,7 +258,7 @@ for word in english_words:
 
 
 if __name__ == "__main__":
-    text = """Robert Frost was born in San Francisco, but his family moved to Lawrence, Massachusetts, in 1884 following his father’s death. The move was actually a return, for Frost’s ancestors were originally New Englanders, and Frost became famous for his poetry’s engagement with New England locales, identities, and themes. Frost graduated from Lawrence High School, in 1892, as class poet (he also shared the honor of co-valedictorian with his wife-to-be Elinor White), and two years later, the New York Independent accepted his poem entitled “My Butterfly,” launching his status as a professional poet with a check for $15.00. Frost's first book was published around the age of 40, but he would go on to win a record four Pulitzer Prizes and become the most famous poet of his time, before his death at the age of 88.
+    text = """Robert Frost was born in San Francisco, but his family moved to Lawrence, Massachusetts, in 1884 following his father's death. The move was actually a return, for Frost’s ancestors were originally New Englanders, and Frost became famous for his poetry’s engagement with New England locales, identities, and themes. Frost graduated from Lawrence High School, in 1892, as class poet (he also shared the honor of co-valedictorian with his wife-to-be Elinor White), and two years later, the New York Independent accepted his poem entitled “My Butterfly,” launching his status as a professional poet with a check for $15.00. Frost's first book was published around the age of 40, but he would go on to win a record four Pulitzer Prizes and become the most famous poet of his time, before his death at the age of 88.
  
 To celebrate his first publication, Frost had a book of six poems privately printed; two copies of Twilight were made—one for himself and one for his fiancee. Over the next eight years, however, he succeeded in having only 13 more poems published. During this time, Frost sporadically attended Dartmouth and Harvard and earned a living teaching school and, later, working a farm in Derry, New Hampshire. But in 1912, discouraged by American magazines’ constant rejection of his work, he took his family to England, where he found more professional success. Continuing to write about New England, he had two books published, A Boy’s Will (1913) and North of Boston (1914), which established his reputation so that his return to the United States in 1915 was as a celebrated literary figure. Holt put out an American edition of North of Boston in 1915, and periodicals that had once scorned his work now sought it. """
     key = generate_key()
