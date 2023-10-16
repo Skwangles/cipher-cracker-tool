9bbbec119d60e917da84f18cdad30c5e5d35a5eb
skwangles <tree_house@Hotmail.co.nz>
2023-10-16 19:52:13 +1300

    Crack by english
    - split every word by their pattern of distinct letters
    - compare every spaced letter with every pattern
    - look through every word in dictionary and see which could match
    - once we have the 'possible' letters, do a collapse
    - slowly remove letters one-by-one that have been 'solved'

--- simple_substitution.py
+++ simple_substitution.py
@@ -1,37 +1,43 @@
 import random
 import string
-
+from alive_progress import alive_bar
 from utils import *
 from nltk.corpus import words 
 
-empty_mapping = {
- 'A': [],
- 'B': [],
- 'C': [],
- 'D': [],
- 'E': [],
- 'F': [],
- 'G': [],
- 'H': [],
- 'I': [],
- 'J': [],
- 'K': [],
- 'L': [],
- 'M': [],
- 'N': [],
- 'O': [],
- 'P': [],
- 'Q': [],
- 'R': [],
- 'S': [],
- 'T': [],
- 'U': [],
- 'V': [],
- 'W': [],
- 'X': [],
- 'Y': [],
- 'Z': []
-}
+ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+def get_empty():
+    
+    return {
+        'A': [],
+        'B': [],
+        'C': [],
+        'D': [],
+        'E': [],
+        'F': [],
+        'G': [],
+        'H': [],
+        'I': [],
+        'J': [],
+        'K': [],
+        'L': [],
+        'M': [],
+        'N': [],
+        'O': [],
+        'P': [],
+        'Q': [],
+        'R': [],
+        'S': [],
+        'T': [],
+        'U': [],
+        'V': [],
+        'W': [],
+        'X': [],
+        'Y': [],
+        'Z': []
+        }
+
 
 
 english_words = None
@@ -134,8 +140,10 @@ def crack(cypher_text):
     
     # Frequency order
     english_freq_order = 'ETAOINSHRDLCUMWFGYPBVKJXQZ'
-    output_mapping = empty_mapping.copy()
-    mapping = empty_mapping.copy()
+    mapping = get_empty()
+    
+    
+    cypher_text = cypher_text.upper().replace("[^A-Z\s]", "")
     
     # first pass - find words that could match each 'word' in the cypher text
     encrypted_words = cypher_text.split(" ")
@@ -145,54 +153,74 @@ def crack(cypher_text):
     
     for c_word in encrypted_words:
         pattern = get_pattern_of_unique(c_word)
+        
         if pattern not in word_patterns:
             continue
-                
-        for possible_word in word_patterns[pattern]:
+        
+        if len(word_patterns[pattern]) == 1:
+            # we have found the word, so add it to the mapping
             for i in range(len(c_word)):
-                if c_word[i] in mapping:
-                    letter = possible_word[i]
-                    if letter not in mapping[c_word[i]]:
-                        mapping[c_word[i]].append(letter)
-
-    sorted_mappings = sorted(mapping.items(), key=lambda x: len(x[1]))
+                mapping[c_word[i]] = [word_patterns[pattern][0][i]]
+            continue
+        
+        single_word_map = get_empty()
+        with alive_bar(len(word_patterns[pattern])) as bar:
+            for possible_word in word_patterns[pattern]:
+                for i in range(len(c_word)):
+                    if c_word[i] in single_word_map:
+                        letter = possible_word[i]
+                        if letter not in single_word_map[c_word[i]]:
+                            single_word_map[c_word[i]].append(letter)
+                bar()
+        mapping = addGuessesToMap(mapping, single_word_map)
     
-    while any(len(x[1]) == 1 for x in sorted_mappings):
-        least = GetShortestEntry(sorted_mappings)
-        if len(least[1]) == 0:
-            # we don't have have any more letters to assign to this mapping, so remove it
-            sorted_mappings.remove(least)
-            continue
-        if len(least[1]) == 1:
-            # we have found the letter for this mapping, so remove it from all other mappings
-            sorted_mappings = remove_letter_from_mapping_dict(sorted_mappings, least[1][0], least[0])
-            sorted_mappings.remove(least)
-            continue
-               
-
-    print(output_mapping)
-
+    mapping = collpase_solved_letters(mapping)
+    print(mapping)
+    
+    
+    
+    
+    
+    
     decrypted = ""
     for letter in cypher_text:
-        if letter.upper() in output_mapping:
-            if letter.isupper():
-                decrypted += output_mapping[letter.upper()][0]
+        if letter.upper() in mapping:
+            if len(mapping[letter.upper()]) > 1 or len(mapping[letter.upper()]) == 0:
+                decrypted += "*"
             else:
-                decrypted += output_mapping[letter.upper()][0].lower()
+                decrypted += mapping[letter.upper()][0]
         else:
             decrypted += letter
 
     return decrypted
 
 
-def remove_letter_from_mapping_dict(mapping, letter, current_letter):
-    """Remove letter which has been assigned from all other 'potential' mappings"""
-    for key in mapping:
-        if key == current_letter:
-            continue
-        if letter in mapping[key]:
-            mapping[key].remove(letter)
+def collpase_solved_letters(mapping):
+    unsolved_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    with alive_bar(len(unsolved_letters)) as bar:
+        while unsolved_letters and any(len(mapping[letter]) == 1 for letter in unsolved_letters):
+            letters_with_1 = [letter for letter in unsolved_letters if len(mapping[letter]) == 1]
+            
+            print(letters_with_1, unsolved_letters)
+            for letter in letters_with_1:
+                if len(mapping[letter]) != 1:
+                    continue
+                
+                bar()
+                
+                solved_letter = mapping[letter][0]
+                
+                unsolved_letters = unsolved_letters.replace(letter, "")
+                
+                # remove solved letter from all other mappings
+                for key in unsolved_letters:
+                    if key == letter:
+                        continue
+                    
+                    if solved_letter in mapping[key]:
+                        mapping[key].remove(solved_letter) 
     return mapping
+            
 
 
 # store the set of words and what they look like globally so we dont have to spend ages getting them each time
@@ -204,8 +232,30 @@ for word in english_words:
         word_patterns[pattern].append(word.upper())
         
 
+def addGuessesToMap(root_map, single_map):
+    """Reduces the root_map to only contain letters that are in both root_map and single_map"""
+    intersection = get_empty()
+    for key in root_map:
+        if key not in single_map:
+            intersection[key] = root_map[key]
+            continue
+        
+        for letter in root_map[key]:
+            if letter in single_map[key]:
+                intersection[key].append(letter)
+        
+        if len(intersection[key]) == 0:
+            # root_map and single_map have no letters in common - join them
+            intersection[key] = root_map[key] + single_map[key]
+        
+    return intersection
+
+
+
 if __name__ == "__main__":
-    text = "This is a super long text string which I will use to test the functionality of the system is it made of various common words that i hope this system can pick our without any issues so please I hope it works this is the best way to check that this test works well the quick brown fox jumps over the lazy dog"
+    text = """Robert Frost was born in San Francisco, but his family moved to Lawrence, Massachusetts, in 1884 following his father’s death. The move was actually a return, for Frost’s ancestors were originally New Englanders, and Frost became famous for his poetry’s engagement with New England locales, identities, and themes. Frost graduated from Lawrence High School, in 1892, as class poet (he also shared the honor of co-valedictorian with his wife-to-be Elinor White), and two years later, the New York Independent accepted his poem entitled “My Butterfly,” launching his status as a professional poet with a check for $15.00. Frost's first book was published around the age of 40, but he would go on to win a record four Pulitzer Prizes and become the most famous poet of his time, before his death at the age of 88.
+ 
+To celebrate his first publication, Frost had a book of six poems privately printed; two copies of Twilight were made—one for himself and one for his fiancee. Over the next eight years, however, he succeeded in having only 13 more poems published. During this time, Frost sporadically attended Dartmouth and Harvard and earned a living teaching school and, later, working a farm in Derry, New Hampshire. But in 1912, discouraged by American magazines’ constant rejection of his work, he took his family to England, where he found more professional success. Continuing to write about New England, he had two books published, A Boy’s Will (1913) and North of Boston (1914), which established his reputation so that his return to the United States in 1915 was as a celebrated literary figure. Holt put out an American edition of North of Boston in 1915, and periodicals that had once scorned his work now sought it. """
     key = generate_key()
     key = "".join(key[letter] for letter in key)
     print("Key:", key)
