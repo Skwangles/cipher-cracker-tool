6b478cc398adfba825ed5920ece835dea332821c
Xander Stokes <xander.c.stokes@gmail.com>
2023-10-18 09:00:28 +1300

    Fix massey batch
    --encrypt/--decrypt/--crack
    needs to come before the cipher type

--- scripts/massey-omura.bat
+++ scripts/massey-omura.bat
@@ -1,10 +1,10 @@
 @echo off
 echo #### Encrypt ####
-python main.py massey --encrypt -t 42 -p 61 -a 3 -b 8 -1 22 -2 44 -3 13
+python main.py --encrypt massey -t 42 -p 61 -a 3 -b 8 -1 22 -2 44 -3 13
 
 echo #### Decrypt ####
-python main.py massey --decrypt -t 22 -p 61 -a 3 -b 8 --step1 22 --step2 44 --step3 13
+python main.py --decrypt -t 22 -p 61 -a 3 -b 8 --step1 22 --step2 44 --step3 13
 
 echo #### Crack ####
-python main.py massey  --crack -t 22 -p 61 -a 3 -b 8
+python main.py --crack massey -t 22 -p 61 -a 3 -b 8
 
