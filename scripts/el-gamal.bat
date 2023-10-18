26a365c4c938097af015c60f42451381b45fbc52
Xander Stokes <xander.c.stokes@gmail.com>
2023-10-19 09:33:04 +1300

    El-gamal batch script - with 42 reference

--- scripts/el-gamal.bat
+++ scripts/el-gamal.bat
@@ -1,10 +1,10 @@
 @echo off
 
 echo #### Encrypt ####
-python main.py  --encrypt elgamal -t 100 -k 13442 -y 1828 -a 30 -p 13757
+python main.py  --encrypt elgamal -t 42 -k 1343 -y 1828 -a 30 -p 13757
 
 echo #### Decrypt ####
-python main.py --decrypt elgamal -t 12380 -t2 3833 -b 9742 -a 30 -p 13757
+python main.py --decrypt elgamal -t 8339 -t2 4519 -b 9742 -a 30 -p 13757
 
 echo #### Crack ####
 python main.py --crack elgamal -t 12380 -t2 3833 -y 1828 -a 30 -p 13757
