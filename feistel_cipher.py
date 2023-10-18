fd418ea91a3a52f81e755af69695520dd2645eb8
Xander Stokes <xander.c.stokes@gmail.com>
2023-10-19 09:26:21 +1300

    remove crack from feistel
    - crack is not useful as it is a pure bruteforce

--- feistel_cipher.py
+++ feistel_cipher.py
@@ -5,14 +5,7 @@ import re
 from string_conversions import *
 from utils import *
 import hashlib
-import itertools
 ALPHABET = "abcdefghijklmnopqrstuvwxyz0123456789"
-
-def generate_combinations(max_length):
-    for length in range(1, max_length + 1):
-        print("- Trying combinations of length " + str(length) + "...")
-        for combo in itertools.product(ALPHABET, repeat=length):
-            yield ''.join(combo)
 
 def get_sub_key(key, round, block_size=16):
     """Generates subkey through a relationship with the main key"""
@@ -97,34 +90,6 @@ def f_func(text, key):
     """The f function used in the Feistal cipher"""
     return xor_string_and_key(text, key)
 
-def crack(cypher_text):
-    """Cracks the cypher text, returning the key - cypher text must be a hex string"""
-    print("Note: If your cypher text is too small the key may be incorrect")
-    print("Cracking the cypher text... If your key is longer than 4 characters you're screwed...")
-    
-    # 36^4 = 1,679,616 combinations, so no chance of cracking a key longer than 4 characters
-    MAX_KEY_CRACK_LENGTH = 4 
-    # 16 rounds is the default, so we'll start there
-    MAX_ROUNDS = 16 
-    
-    # Check if the text is already decrypted - some one might have put in the wrong thing
-    initial_conversion = str(binary_to_string(hex_to_binary(cypher_text)))
-    if initial_conversion.isalnum() and index_of_coincidence(initial_conversion) > 0.066:
-        print("No key needed - text is already decrypted")
-        return binary_to_string(cypher_text)
-    
-    for i in range(MAX_ROUNDS, 1, -1):
-        print("Trying with " + str(i) + " rounds...")
-        for key in generate_combinations(MAX_KEY_CRACK_LENGTH):
-            decrypted = str(decrypt(cypher_text, key, i))
-            if re.match("^[a-zA-Z0-9\\s]*$", decrypted) and index_of_coincidence(decrypted) >= 0.066:
-                # Finish the loading bars
-                print("Done")
-                return "Key: " + key + ", Plain Text: " + decrypted
-                    
-    return "No key found in the reasonable limits we've set - increase MAX_KEY_CRACK_LENGTH or MAX_ROUNDS"
-
-
 
 if __name__ == "__main__":
     print("Feistel cipher")
@@ -137,7 +102,4 @@ if __name__ == "__main__":
     print("------------------")
     print("Decrypting with key")
     print(decrypt(cypher_text, key, 16))
-    
-    print("Cracking")
-    print(crack(cypher_text))
     
