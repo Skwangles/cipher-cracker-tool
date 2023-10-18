23d8b86ce517dd165521aba47d0915946dac9974
Xander Stokes <xander.c.stokes@gmail.com>
2023-10-19 09:42:42 +1300

    guess the number -rsa

--- scripts/rsa.bat
+++ scripts/rsa.bat
@@ -1,11 +1,16 @@
 @echo off
 
 echo #### Encrypt ####
-python main.py  --encrypt rsa -t 100 -n 58687709 -e 270679
+python main.py  --encrypt rsa -t 95 -n 58687709 -e 270679
 
 echo #### Decrypt ####
-python main.py  --decrypt rsa -t 41802438 -n 58687709 -d 5419
+python main.py  --decrypt rsa -t 37012695 -n 58687709 -d 5419
 
 echo #### Crack ####
 python main.py --crack rsa -t 41802438 -n 58687709 -e 270679
 
+echo #### Crack - Fermats Factorization Method ####
+python main.py --crack rsa -t 41802438 -n 88679873 -e 270679
+
+echo #### Crack - Bruteforce ####
+python main.py --crack rsa -t 41802438 -n 88679873 -e 270679
