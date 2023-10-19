9cdee8aab2ee2ae49390cbf793880005c05fecc0
Xander Stokes <xander.c.stokes@gmail.com>
2023-10-19 23:59:00 +1300

    Commenting and polishing massey_omura
    - Making sure comments are easy to understand
    - Properly 'demoing' the 3 stages of the algorithm

--- massey_omura.py
+++ massey_omura.py
@@ -1,58 +1,81 @@
 import utils
 
 def encrypt(m, a, b, p):
-    """Encrypts the text using the key"""
-    # Get the inverse of a and b - private information
-    a_inv = utils.modInverse(a, p-1) 
-    b_inv = utils.modInverse(b, p-1) 
-    
-    # Step 1 - Alice sends m to Bob
-    m_a = pow(m, a, p)
-    
-    # Step 2 - Alice sends m_a to Bob
-    m_ab = pow(m_a, b, p)
-    
-    # Step 3 - Alice multiplies by her inverse to get m_b
-    m_b = pow(m_ab, a_inv, p)
-    
+    """Simulates the first 3 steps of the Massey-Omura protocol - A (sender) and B (receiver) transmitting a message m"""
+    print("Simulating Massey-Omura...")
 
-    print("y1 (A -> B):", m_a)
-    print("y2 (A <- B):", m_ab)
-    print("y3 (A -> B):")
-    return m_b
+    # Step 0 - A and B calculates their own inverses (kept private)
+    a_inv = utils.modInverse(a, p-1) 
+    # b_inv = ... B's inverse is not needed for this 'encryption' function
+    
+    y1 = calculate_y1(m, a, p)
+    print("y1 (A -> B):", y1)
+    
+    y2 = calculate_y2(y1, b, p)
+    print("y2 (A <- B):", y2)
+    
+    y3 = calculate_y3(y2, a_inv, p)
+    print("y3 (A -> B):", y3)
+
+    print("Note: Receiver can now decrypt the following message using his inverse")
+    return y3
 
 
 def decrypt(m_b, b, p, b_inv=-1):
-    """Decrypts the cypher text using the m^b"""
+    """Decrypts m^b mod p using the receiver's private key to calculate the inverse and generate the key - p-1 is used as the modulus"""
+    
+    print("Receiver calculates his inverse...")
     if b_inv == -1:
         b_inv = utils.modInverse(b, p-1)
-    return pow(m_b, b_inv, p)
+    
+    print("Decrypting m...")
+    return calculate_m_from_y3(m_b, b_inv, p)
 
 
 def crack(m_a, m_ab, m_b, p):
-    """Cracks the cypher text, returning the key"""
+    """Cracks the message m, and A & B's private keys"""
     
-    # Uses a brute force method to find the key or 'discrete log' solver
+    # Uses a brute force method to find the key ('discrete logarithm problem')
     b_mod_p = find_key(m_a, m_ab, p)
     a_mod_p = find_key(m_b, m_ab, p)
+    
     if a_mod_p == -1 or b_mod_p == -1:
         return "Could not determine a or b"
 
     print("A key:", a_mod_p)
     print("B key:", b_mod_p)
-    print("Cracked message:")    
+    
+    # Decrypt the message using the cracked B
+    print("Cracked message:") 
     return decrypt(m_b, b_mod_p, p)
+
+def calculate_y1(m, a, p):
+    """Calculates y1 (A -> B) - A sends m^a mod p to B"""
+    return pow(m, a, p)
+
+def calculate_y2(y1, b, p):
+    """Calculates y2 (A <- B) - B sends y1^b mod p to A (which is now m^ab mod p)"""
+    return pow(y1, b, p)
+
+def calculate_y3(y2, a_inv, p):
+    """Calculates y3 (A -> B) - A sends y2^(a^-1) mod p to B (which is now m^b mod p) and can be decrypted by B's inverse"""
+    return pow(y2, a_inv, p)
+
+def calculate_m_from_y3(y3, b_inv, p):
+    """Calculates m (A <- B) - B decrypts y3 using his private key's inverse"""
+    return pow(y3, b_inv, p)
 
 
 def find_key(m_a, m_ab, p):
-    """Gets the power """
+    """Brute forces the discrete logarithm problem to find the key"""
     if m_a == m_ab:
         return 1
-    
-    # Discrete log solver - brute force powers to p-1
-    for i in range(2, p):
+
+    # Check powers 1 to p-1
+    for i in range(1, p):
         if (pow(m_a, i, p)) == m_ab:
             return i
+        
     return -1
 
 if __name__ == "__main__":
