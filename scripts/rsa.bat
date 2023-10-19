245e1c0a1564ded4f396a4bfd0563d4e7b46eb42
Xander Stokes <xander.c.stokes@gmail.com>
2023-10-19 23:35:48 +1300

    updating rsa bat with weak key & flags

--- scripts/rsa.bat
+++ scripts/rsa.bat
@@ -7,10 +7,13 @@ echo #### Decrypt ####
 python main.py  --decrypt rsa -t 37012695 -n 58687709 -d 5419
 
 echo #### Crack ####
-python main.py --crack rsa -t 41802438 -n 58687709 -e 270679
+python main.py --crack rsa -t 41802438 -n 58687709 -e 270679 --factor-db
 
-echo #### Crack - Fermats Factorization Method ####
-python main.py --crack rsa -t 41802438 -n 88679873 -e 270679
+echo ### Generate Random Key (p, q, d, e, n) ###
+python main.py --generate rsa --min 100 --max 10000
 
-echo #### Crack - Bruteforce ####
-python main.py --crack rsa -t 41802438 -n 88679873 -e 270679
+echo ### Generate Weak Key (p, q, d, e, n) - Close to together ###
+python main.py --generate rsa --min 100 --max 10000 --weak
+
+echo ### Crack weak key ###
+python main.py --crack rsa -t 17690470 -n 69222319 -e 270679
